1ba5b9ead9ca290e4883bd4df78517628af8a4f9
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2025-12-11 21:00:11 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,13 +1,17 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python salary.py <salary>")
-    sys.exit(1)
+default_salary = 30000.0
 
-salary = float(sys.argv[1])
+if len(sys.argv) == 2:
+    salary = float(sys.argv[1])
+    source = "User Input"
+else:
+    salary = default_salary
+    source = "Self-assigned"
 
 bonus = salary * 0.10
 total_salary = salary + bonus
 
+print(f"Salary: {salary} ({source})")
 print("Bonus Amount:", bonus)
 print("Total Salary after Bonus:", total_salary)
